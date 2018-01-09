6b36a402c46208762b66c90e884600ac71b824a5
Bartosz Bułaj <bartoszbulaj@gmail.com>
2018-01-09 15:04:35 +0100

    zmiana layout na constraint

--- app/src/main/java/com/team/boo/cryptocointicker/MainActivity.java
+++ app/src/main/java/com/team/boo/cryptocointicker/MainActivity.java
@@ -1,13 +1,8 @@
 package com.team.boo.cryptocointicker;
 
-import android.content.Intent;
-import android.os.CountDownTimer;
 import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,13 +13,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.team.boo.cryptocointicker.model.BitApi;
-import com.team.boo.cryptocointicker.model.Kursy;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 
@@ -32,36 +24,24 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String mTAG = MainActivity.class.getName();
     private RequestQueue mRequestQueue;
-    private StringRequest stringRequest;
-    private TextView odpowiedzTextView;
-    private Button przycisk;
-    private Kursy kursObecny;
+
+    private TextView odpTextView;
+    private Button startButton;
     private BitApi bitfinexObj;
     private JsonObjectRequest job;
     private Handler handler;
 
-    private String url1 = "https://bitbay.net/API/Public/LSKPLN/ticker.json";
-    private String currency1 = "LSK: \n";
-    private String url2 = "https://bitbay.net/API/Public/BTCPLN/ticker.json";
-    private String currency2 = "BTC: \n";
-    private String url3 = "https://bitbay.net/API/Public/BCCPLN/ticker.json";
-    private String currency3 = "BCC: \n";
-    private String url4 = "https://bitbay.net/API/Public/LTCPLN/ticker.json";
-    private String currency4 = "LTC: \n";
-    private String url5 = "https://bitbay.net/API/Public/GAMEPLN/ticker.json";
-    private String currency5 = "GAME: \n";
-    private String url6 = "https://bitbay.net/API/Public/ETHPLN/ticker.json";
-    private String currency6 = "ETH: \n";
-
-    private final String bitfinexUrl = "https://api.bitfinex.com/v1/";
-    private final String bitfinexBtcUsdString = "pubticker/btcusd";
+    private final String bitfinexUrl = "https://api.bitfinex.com/v1/pubticker/";
+    private final String btcusdString = "btcusd";
+    private final String bchusdString = "bchusd";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        przycisk = (Button) findViewById(R.id.myButton);
-        odpowiedzTextView = (TextView) findViewById(R.id.myText);
+
+        startButton = (Button) findViewById(R.id.startButton);
+        odpTextView = (TextView) findViewById(R.id.odpTextView);
         mRequestQueue = Volley.newRequestQueue(this);
         handler = new Handler();
 
@@ -75,8 +55,6 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(JSONObject response) {
                         Gson gson = new Gson();
                         bitfinexObj = gson.fromJson(response.toString(), BitApi.class);
-//                        String s = bitfinexObj.getLastPrice().toString();
-//                        odpowiedzTextView.setText(s);
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -88,17 +66,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void myButtonClicked(View view) throws InterruptedException {
-        odpowiedzTextView.setText("Bitfinex Public REST\n\n");
+        //odpTextView.setText("Bitfinex Public REST\n\n");
 
-        getObjBitF(bitfinexUrl, bitfinexBtcUsdString);
+        getObjBitF(bitfinexUrl, btcusdString);
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 String s = bitfinexObj.getLastPrice().toString();
-                odpowiedzTextView.append(s);
+                odpTextView.append(s);
             }
         },1000);
-
 
         //  Intent i = new Intent(this, MyCheckService.class);
         //  startService(i);
